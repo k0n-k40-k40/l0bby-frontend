85fb83c8d6dca1c26abf6f8baa2be00b86ea3989
nkwn <npquan2704@gmail.com>
2024-02-24 02:39:06 +0700

    Add room-listing layout

--- lib/src/components/all_buttons.dart
+++ lib/src/components/all_buttons.dart
@@ -3,24 +3,19 @@ import 'tonal_button.dart';
 
 class AllModeButtons extends StatelessWidget {
   const AllModeButtons({
-    Key ? key,
+    Key? key,
   }) : super(key: key);
 
   BoxDecoration myBoxDecoration() {
     return const BoxDecoration(
       borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(40.0),
-        topRight: Radius.circular(40.0)
-      ),
+          topLeft: Radius.circular(40.0), topRight: Radius.circular(40.0)),
       color: Color.fromRGBO(255, 255, 255, 0.4),
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
@@ -33,30 +28,27 @@ class AllModeButtons extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TonalButton(
-              text: "Automatic!", 
-              backgroundColor: Colors.lightBlueAccent, 
+              text: "Automatic!",
+              backgroundColor: Colors.lightBlueAccent,
               onPressed: () {
                 Navigator.pushNamed(context, '/automatic');
               },
             ),
             TonalButton(
-              text: "Manual!", 
-              backgroundColor: const Color.fromRGBO(64, 196, 255, 1), 
+              text: "Manual!",
+              backgroundColor: Colors.lightBlueAccent,
               onPressed: () {
                 Navigator.pushNamed(context, '/manual');
-
               },
             ),
             TonalButton(
-              text: "Create Room!", 
-              backgroundColor: Colors.lightBlueAccent, 
-              onPressed: () {
-
-              },
+              text: "Create Room!",
+              backgroundColor: Colors.lightBlueAccent,
+              onPressed: () {},
             ),
             TonalButton(
-              text: "Go to auth page!", 
-              backgroundColor: Colors.lightBlueAccent, 
+              text: "Go to auth page!",
+              backgroundColor: Colors.lightBlueAccent,
               onPressed: () {
                 Navigator.pushNamed(context, '/auth');
               },
--- lib/src/screens/manual_match.dart
+++ lib/src/screens/manual_match.dart
@@ -7,19 +7,96 @@ class ManualMatchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Manual Matching')),
-      bottomNavigationBar: const CustomNavigationBar(
-        currentIndex: 2,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/');
-          },
-          child: const Text('Go to Home Page'),
-        ),
+      appBar: buildAppBar(),
+      body: Column(
+        children: [
+          buildSearch(),
+          Container(
+            height: 160.0,
+            child: Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(),
+                Material(
+                  elevation: 3.0,
+                  child: Container(
+                    height: 100.0,
+                    margin: EdgeInsets.only(left: 150.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                "Room 1",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: 150.0,
+                    height: 130.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://img.freepik.com/premium-vector/football-soccer-logo_609550-353.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+buildAppBar() {
+  return AppBar(
+      backgroundColor: Colors.lightBlueAccent,
+      title: const Text('Manual Match',
+          style: TextStyle(fontWeight: FontWeight.bold)),
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.refresh),
+          onPressed: () {},
+        ),
+      ]);
+}
+
+Padding buildSearch() {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+      children: <Widget>[
+        Expanded(
+            child: TextField(
+          decoration: InputDecoration(
+            hintText: "Search...",
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 17.0),
+            prefixIcon: Icon(Icons.search),
+          ),
+        )),
+        IconButton(icon: Icon(Icons.filter_list), onPressed: () {}),
+      ],
+    ),
+  );
 }

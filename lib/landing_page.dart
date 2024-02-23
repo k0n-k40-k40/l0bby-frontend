13f9f6c8a4ed4dca19107a0a3ed87c4da4ac7bf8
Duy Le <duyla25clc@gmail.com>
2024-02-23 18:01:25 +0700

    adding image

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -7,8 +7,79 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hello world"),
+        title: const Text("l0bby"),
+      ),
+      backgroundColor: const Color.fromARGB(255, 74, 91, 106),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            HelloBar(username: "duy"),
+            SizedBox(height: 20,),
+            HomeImage(source: "lib/assets/running_tree.png"),
+          ]
+        )
+      ),
+    );   
+  }
+}
+
+class HelloBar extends StatelessWidget {
+  const HelloBar({
+    Key? key,
+    required this.username,
+  }) : super(key: key);
+
+  final String username;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final textSize = screenWidth * 0.07;
+
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        // minHeight: 100, 
+      ),
+      child: Container (
+        color: Colors.red,
+        child: Text(
+          "hello, $username!!\n Lets play some games!!",
+          style: TextStyle(fontSize: textSize, ),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
 }
+
+class HomeImage extends StatelessWidget {
+  const HomeImage({
+    Key? key,
+    required this.source,
+  }) : super(key: key);
+
+  final String source;
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(),
+      child: Center(
+        child: Image.asset((source)),
+      ),
+    );
+  }
+}
+
+// class AllModeButtons extends StatelessWidget {
+//   const AllModeButtons({
+//     Key ? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column()
+//   }
+
+// }

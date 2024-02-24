aee025c0d948cd205cc56c584e8705ff6679533a
Duy Le <duyla25clc@gmail.com>
2024-02-25 02:11:12 +0700

    core require completed

---
+++ lib/src/screens/booking_page.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import '../components/stars_rating.dart';
+
+class CourtInfo extends StatelessWidget {
+  final String name;
+  final String address;
+  final double rating;
+  final String phoneNumber;
+
+  const CourtInfo ({
+    super.key,
+    required this.name,
+    required this.address,
+    required this.rating,
+    required this.phoneNumber
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            name,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.normal
+            ),
+          ),
+          Text(
+            address,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.5),
+              fontSize: 13
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5),
+            child: StarRating(
+              rating: rating
+            ),
+          )
+        ],
+      );
+  }
+}
+
+
+_showNotification(BuildContext context, String phoneNumber) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text('Notification'),
+        content: Text('Do you want to call $phoneNumber?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+class CallButton extends StatelessWidget {
+  final String phoneNumber;
+
+  const CallButton({
+    super.key,
+    required this.phoneNumber
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () => {
+          _showNotification(context, phoneNumber),
+        },
+        child: const Text(
+          "Book this court!!",
+        )
+      );
+  }
+
+}
+
+class BookingPage extends StatelessWidget {
+
+  final String backgroundFileName;
+  final CourtInfo courtInfo;
+
+  const BookingPage({
+    super.key,
+    required this.backgroundFileName,
+    required this.courtInfo
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            // Background image
+            Image.asset(
+              backgroundFileName,
+
+              fit: BoxFit.cover,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+            // Content on top of the background image goes from here down
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(padding: EdgeInsets.all(20), child:
+                  CallButton(phoneNumber: courtInfo.phoneNumber),
+                ),
+                Expanded(child: Container()),
+                Padding(padding: EdgeInsets.only(left: 20, bottom:  20),
+                  child: courtInfo,
+                )
+              ],
+            ),
+          ],
+      )
+    );
+  }
+
+}
--- lib/test.dart
+++ lib/test.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
-import 'src/components/date_picker.dart';
+import 'src/screens/booking_page.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: DatePicker(width: 300, height: 50, radius: 30,),
-        ),
-      ),
+      home: BookingPage(
+        backgroundFileName: 'lib/assets/courts/court_1.jpg',
+        courtInfo: CourtInfo(
+          name: "Sân Cầu Lông Cao Lỗ",
+          address: "45 Cao Lỗ, Phương 4, Quận 8, Thành phố Hồ Chí Minh",
+          rating: 3.5,
+          phoneNumber: "09090909090",
+          ),
+      )
     );
   }
 }

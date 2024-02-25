e49594d446727f4c5dbdeec968f0042b538bafbc
Vbeelearncode <baovg.a1.k2023@gmail.com>
2024-02-25 13:17:26 +0700

    added manual room list screen

---
+++ lib/src/screens/room_list.dart
@@ -0,0 +1,234 @@
+// ignore_for_file: sort_child_properties_last
+
+import 'package:flutter/material.dart';
+import '../components/navigation_bar.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class RoomListScreen extends StatelessWidget {
+  const RoomListScreen({super.key});
+
+  void _showJoinRoomDialog(BuildContext context) {
+    final TextEditingController _roomCodeController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Join a Room'),
+          content: TextField(
+            controller: _roomCodeController,
+            decoration: const InputDecoration(hintText: 'Enter room code'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Join'),
+              onPressed: () {
+                // Here, you would usually validate the code and navigate to the room
+                // For demonstration purposes, just printing to console and closing the dialog
+                // Fluttertoast.showToast(
+                //   msg: 'Joining room...',
+                //   toastLength: Toast.LENGTH_SHORT,
+                //   gravity: ToastGravity.BOTTOM,
+                // );
+                Navigator.pushNamed(context, '/lobby');
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> rooms = [
+      {
+        'id': 123,
+        'sportName': 'Cầu lông',
+        'location': 'Trung tâm thể dục A',
+        'startTime': '10:00',
+        'endTime': '12:00',
+        'date': '22/10/2023',
+      },
+      {
+        'id': 345,
+        'sportName': 'Cầu lông',
+        'location': 'Công viên Tao Đàn',
+        'startTime': '10:00',
+        'endTime': '12:00',
+        'date': '01/01/2024',
+      },
+      {
+        'id': 546,
+        'sportName': 'Bóng đá',
+        'location': 'Sân vận động B',
+        'startTime': '15:00',
+        'endTime': '17:00',
+        'date': '23/10/2023',
+      },
+      {
+        'id': 779,
+        'sportName': 'Bóng rổ',
+        'location': 'Trung tâm thể dục C',
+        'startTime': '13:00',
+        'endTime': '15:00',
+        'date': '24/10/2023',
+      },
+      {
+        'id': 101,
+        'sportName': 'Quần vợt',
+        'location': 'Sân quần vợt D',
+        'startTime': '08:00',
+        'endTime': '10:00',
+        'date': '25/10/2023',
+      },
+      {
+        'id': 202,
+        'sportName': 'Bóng chuyền',
+        'location': 'Trung tâm thể dục E',
+        'startTime': '18:00',
+        'endTime': '20:00',
+        'date': '26/10/2023',
+      },
+      {
+        'id': 303,
+        'sportName': 'Bơi lội',
+        'location': 'Bể bơi F',
+        'startTime': '06:00',
+        'endTime': '08:00',
+        'date': '27/10/2023',
+      },
+      {
+        'id': 404,
+        'sportName': 'Gym',
+        'location': 'Trung tâm thể dục G',
+        'startTime': '09:00',
+        'endTime': '11:00',
+        'date': '28/10/2023',
+      },
+      {
+        'id': 505,
+        'sportName': 'Yoga',
+        'location': 'Trung tâm Yoga H',
+        'startTime': '07:00',
+        'endTime': '09:00',
+        'date': '29/10/2023',
+      },
+      {
+        'id': 606,
+        'sportName': 'Nhảy',
+        'location': 'Trung tâm nhảy I',
+        'startTime': '16:00',
+        'endTime': '18:00',
+        'date': '30/10/2023',
+      },
+      {
+        'id': 707,
+        'sportName': 'Quyền Anh',
+        'location': 'Trung tâm quyền Anh J',
+        'startTime': '14:00',
+        'endTime': '16:00',
+        'date': '31/10/2023',
+      },
+      {
+        'id': 808,
+        'sportName': 'Karate',
+        'location': 'Trung tâm Karate K',
+        'startTime': '17:00',
+        'endTime': '19:00',
+        'date': '01/11/2023',
+      }
+    ];
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E1E1E),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF333333),
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Danh sách phòng', style: TextStyle(color: Colors.white)),
+            Text(
+              'Các hoạt động thể thao đang diễn ra',
+              style: TextStyle(color: Colors.grey, fontSize: 14),
+            ),
+          ],
+        ),
+        iconTheme: const IconThemeData(color: Colors.white),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              // Handle search action
+            },
+          ),
+          PopupMenuButton<int>(
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              const PopupMenuItem<int>(value: 0, child: Text('Option 1')),
+              const PopupMenuItem<int>(value: 1, child: Text('Option 2')),
+            ],
+            onSelected: (item) => {
+              // Handle item selection
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: const CustomNavigationBar(currentIndex: 2),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showJoinRoomDialog(context),
+        child: const Icon(Icons.vpn_key),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: rooms.length,
+        separatorBuilder: (_, __) => const Divider(color: Colors.grey),
+        itemBuilder: (context, index) {
+          final room = rooms[index];
+          return Card(
+            color: const Color(0xFF333333),
+            child: ListTile(
+              onTap: () {
+                // navigate to /lobby
+                Navigator.pushNamed(context, '/lobby');
+              },
+              leading: Text(
+                '#${room['id']}',
+                style: const TextStyle(color: Colors.white, fontSize: 14),
+                overflow: TextOverflow.ellipsis, // Add ellipsis for overflow
+              ),
+              title: Text(
+                'Chơi ${room['sportName']} tại ${room['location']}',
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+                overflow: TextOverflow.ellipsis, // Add ellipsis for overflow
+                maxLines: 1, // Ensure title is limited to a single line
+              ),
+              subtitle: Text(
+                '${room['startTime']} -> ${room['endTime']} ${room['date']}',
+                style: const TextStyle(color: Colors.grey, fontSize: 14),
+                overflow: TextOverflow.ellipsis, // Add ellipsis for overflow
+                maxLines: 2, // Allow for two lines in the subtitle
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+          );
+        },
+      ),
+    );
+  }
+}
